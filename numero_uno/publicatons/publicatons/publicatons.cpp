1b2aa0b3f8c779468b9b58f074a9b1d3ec7e84d1
Алиев Расим Рауфович <1arr@spbt.local>
2023-02-11 12:36:52 +0300

    added sort

--- numero_uno/publicatons/publicatons/publicatons.cpp
+++ numero_uno/publicatons/publicatons/publicatons.cpp
@@ -2,10 +2,188 @@
 //
 
 #include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+
+class   publication
+{
+private:
+    string name;
+    int year;
+
+public:
+    string setName(string sName) {
+
+    };
+    string getName() {
+        return name;
+    };
+    int setYear(int sYear) {
+        int error_code{0};
+        if (year <= 2014 || year > 2023) {
+            error_code = -1;
+        }
+        else {
+            year = sYear;
+        };
+        return  error_code;
+    };
+    int getYear() {
+
+        return year;
+    };
+        
+};
+
+class book : public publication {
+private:
+    string ISBN;
+    string izd;
+    string auth;
+    int pubC;
+public:
+    string setISBN(string sISBN) {
+
+    };
+    string getISBN() {
+        return ISBN;
+    };
+    string setizd(string sizd) {
+
+    };
+    string getizd() {
+        return izd;
+    };
+    string setauth(string sauth) {
+
+    };
+    string getauth() {
+        return auth;
+    };
+    int setpubC(string spubC) {
+
+    };
+    int getpubC() {
+        return pubC;
+    };
+   
+};
+
+class magazine : public publication {
+private:
+    string ISBN;
+    string izd;
+    string auth;
+    int number;
+    int tom;
+    int pubC;
+    bool scoup;
+    bool WoS;
+    bool RINC;
+public:
+    string setISBN(string sISBN) {
+
+    };
+    string getISBN() {
+        return ISBN;
+    };
+    string setizd(string sizd) {
+
+    };
+    string getizd() {
+        return izd;
+    };
+    string setauth(string sauth) {
+
+    };
+    string getauth() {
+        return auth;
+    };
+    int setnumber(string snumber) {
+
+    };
+    int getnumber() {
+        return number;
+    };
+    int settom(string stom) {
+
+    };
+    int gettom() {
+        return tom;
+    };
+    int setpubC(string spubC) {
+
+    };
+    int getpubC() {
+        return pubC;
+    };
+    bool setscoup(string sscoup) {
+
+    };
+    bool getscoup() {
+        return scoup;
+    };
+    bool setWoS(string sWoS) {
+
+    };
+    bool getWoS() {
+        return WoS;
+    };
+    bool setRINC(string sRINC) {
+
+    };
+    bool getRINC() {
+        return RINC;
+    };
+
+};
+
+class article : public magazine {
+private:
+    string auth;
+    int Pstart;
+    int Pend;
+    bool stat;
+
+public:
+    string setauth(string sauth) {
+
+    };
+    string getauth() {
+        return auth;
+    };
+    int setPstart(string sPstart) {
+
+    };
+    int getPstart() {
+        return Pstart;
+    };
+    int setPend(string sPend) {
+
+    };
+    int getPend() {
+        return Pend;
+    };
+    bool setstat(string sstat) {
+
+    };
+    bool getstat() {
+        return stat;
+    };
+};
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
     std::cout << "Hello World!\n";
+    publication a;
+    cout << "Введите год: ";
+    int b;
+    cin >> b;
+    cout<<a.setYear(b);
+    
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
